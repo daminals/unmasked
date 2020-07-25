a03ba5c57a7168331454a6607e9f26261772ca68
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-07-25 11:55:42 -0400

    shorten le py

--- protect.py
+++ protect.py
@@ -1,16 +1,13 @@
 # protect.py
 # Daniel Kogan
 # 7/25/2020
-
 import fire
 from PIL import Image
 import numpy as np
-
 def main(IMG):
 	mask = Image.open("mask.png")
 	background = Image.open(IMG)
 	background.paste(mask, (0,0), mask)
 	background.save(IMG)
-
 if __name__ == '__main__':
 	fire.Fire(main)
